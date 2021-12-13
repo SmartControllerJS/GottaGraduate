783069dee2c11e616295631025945c7e9cafd7ba
fraser-dempster <2331229d@student.gla.ac.uk>
2021-12-13 21:55:13 +0000

    added ability to flap bird

--- src/index.js
+++ src/index.js
@@ -10,10 +10,10 @@ const config = {
     // Arcade physics plugin, manages physics simulation
     default: 'arcade',
     arcade: {
-      // gravity: {
-      //   y: 200
-      // }
-      // debug: true
+      gravity: {
+        y: 400
+      },
+      debug: true
     }
   },
   scene: {
@@ -30,32 +30,29 @@ function preload() {
   this.load.image('sky', 'assets/sky.png');
   this.load.image('bird', 'assets/bird.png');
 }
+
 const VELOCITY = 200;
 let bird = null;
+let flapVelocity = 250;
 
 function create() {
   // 0,0 is the topleft of the screen
-  // x - coordinate
-  // y - coordinate
-  // key of the image
   // default origin point is 0.5 0.5 which is the middle
   this.add.image(0, 0, 'sky').setOrigin(0, 0);
-  // when no image is loaded a default sqaure is put into the scene
   bird = this.physics.add.sprite(config.width / 10, config.height / 2, 'bird').setOrigin(0);
-  // bird.body.gravity.y = 200;
-  bird.body.velocity.x = VELOCITY;
+  
+  this.input.on('pointerdown', flap);
+
+  this.input.keyboard.on('keydown_SPACE', flap);
 }
 
 
 
-// 60fps
-// 60 times per second update will be executed
 function update() {
-  if (bird.x >= config.width - bird.width) {
-    bird.body.velocity.x = -VELOCITY;
-  }
-  else if (bird.x <= 0) {
-    bird.body.velocity.x = VELOCITY;
-  }
+
+}
+
+function flap() {
+  bird.body.velocity.y = -flapVelocity;;
 }
 new Phaser.Game(config);
