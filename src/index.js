c5fbebe1281f040b71cc432bbac444165ae907db
fraser-dempster <2331229d@student.gla.ac.uk>
2021-12-13 20:32:31 +0000

    set origin point to 0 0

--- src/index.js
+++ src/index.js
@@ -28,6 +28,7 @@ function create() {
   // x - coordinate
   // y - coordinate
   // key of the image
-  this.add.image(config.width / 2, config.height / 2, 'sky');
+  // default origin point is 0.5 0.5 which is the middle
+  this.add.image(0, 0, 'sky').setOrigin(0, 0);
 }
 new Phaser.Game(config);
