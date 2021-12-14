3fe465cc4cd262e03a4f52d00157dfa244b4f39f
fraser-dempster <2331229d@student.gla.ac.uk>
2021-12-14 20:58:09 +0000

    added shared config for multiple scenes

--- src/scenes/PlayScene.js
+++ src/scenes/PlayScene.js
@@ -2,12 +2,9 @@ import Phaser from "phaser";
 
 class PlayScene extends Phaser.Scene {
 
-  constructor() {
+  constructor(config) {
     super('PlayScene');
-    this.initialBirdPOsition = {
-      x: 80,
-      y:300
-    }
+    this.config = config;
 
     this.bird = null;
   }
@@ -19,7 +16,7 @@ class PlayScene extends Phaser.Scene {
 
   create() {
     this.add.image(0, 0, 'sky').setOrigin(0, 0);
-    this.bird = this.physics.add.sprite(this.initialBirdPOsition.x,this.initialBirdPOsition.y, 'bird').setOrigin(0);
+    this.bird = this.physics.add.sprite(this.config.startPosition.x, this.config.startPosition.y, 'bird').setOrigin(0);
     this.bird.body.gravity.y = 400;
   }
 
