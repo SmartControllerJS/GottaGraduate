15f7116be5e6fc527d218dce064f6bb47ea213eb
fraser-dempster <2331229d@student.gla.ac.uk>
2021-12-14 21:46:32 +0000

    finished refactoring

--- src/scenes/PlayScene.js
+++ src/scenes/PlayScene.js
@@ -26,32 +26,45 @@ class PlayScene extends Phaser.Scene {
   }
 
   create() {
+    this.createBG();
+    this.createBird();
+    this.createPipes();
+    this.handleInputs();
+  }
+
+  update() {
+    this.checkGameStatus();
+    this.recyclePipes();
+  }
+
+  createBG() {
     this.add.image(0, 0, 'sky').setOrigin(0, 0);
+  }
+
+  createBird() {
     this.bird = this.physics.add.sprite(this.config.startPosition.x, this.config.startPosition.y, 'bird').setOrigin(0);
     this.bird.body.gravity.y = 400;
+  }
 
-    this.pipes = this.physics.add.group(); // dynamic group
-
+  createPipes() {
+    this.pipes = this.physics.add.group();
     for (let i = 0; i < PIPES_TO_RENDER; i++) {
       const upperPipe = this.pipes.create(0, 0, 'pipe').setOrigin(0, 1);
       const lowerPipe = this.pipes.create(0, 0, 'pipe').setOrigin(0, 0);
-  
       this.placePipe(upperPipe, lowerPipe);
     }
-  
     this.pipes.setVelocityX(-200);
-    
-    
+  }
+
+  handleInputs() {
     this.input.on('pointerdown', this.flap, this);
     this.input.keyboard.on('keydown_SPACE', this.flap, this);
   }
 
-  update() {
+  checkGameStatus() {
     if (this.bird.y > this.config.height || this.bird.y < -this.bird.height) {
       this.restartBirdPosition();
     }
-  
-    this.recyclePipes();
   }
 
   placePipe(uPipe, lPipe) {
