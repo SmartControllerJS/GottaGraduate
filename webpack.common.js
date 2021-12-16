81e87b780290ab1e70be5c449e7e50b63401a598
fraser-dempster <2331229d@student.gla.ac.uk>
2021-12-16 13:44:38 +0000

    fixing github pagse

--- webpack.common.js
+++ webpack.common.js
@@ -36,7 +36,7 @@ module.exports = {
     ]
   },
   devServer: {
-    contentBase: path.resolve(__dirname, 'build'),
+    contentBase: path.resolve(__dirname, 'docs'),
     compress: true,
     port: 8080,
   },
@@ -52,7 +52,7 @@ module.exports = {
       patterns: [
         {
           from: path.resolve(__dirname, 'assets'),
-          to: path.resolve(__dirname, 'build/assets')
+          to: path.resolve(__dirname, 'docs/assets')
         }
       ],
     })
