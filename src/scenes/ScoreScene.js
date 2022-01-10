b8b37e51d78df3b9ff1be6407b524299634bba9b
fraser-dempster <2331229d@student.gla.ac.uk>
2022-01-10 20:28:04 +0000

    added back button to score scene

--- src/scenes/ScoreScene.js
+++ src/scenes/ScoreScene.js
@@ -3,7 +3,7 @@ import BaseScene from './BaseScene';
 class ScoreScene extends BaseScene {
 
   constructor(config) {
-    super('ScoreScene', config);
+    super('ScoreScene', {...config, canGoBack: true});
 
   }
 
