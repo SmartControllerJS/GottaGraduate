117a7b9c2c3cdcba5d53894fa64088cd5c84d356
Fraser Dempster <fraserdempster23@gmail.com>
2022-04-15 03:32:55 +0100

    fixed last game timer

--- src/scenes/PlayScene.js
+++ src/scenes/PlayScene.js
@@ -163,7 +163,7 @@ class PlayScene extends Phaser.Scene {
         loop: false
       });
       this.text.setVisible(true);
-      this.time.addEvent({ delay: 15000, callback: this.onEvent, callbackScope: this, loop: true });
+      this.time.addEvent({ delay: 150000, callback: this.onEvent, callbackScope: this, loop: true });
       this.time.addEvent({ delay: 1000, callback: this.gameover, callbackScope: this, loop: false });
     }
     this.checkPlayersScore();
